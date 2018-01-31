612841b9ec48c6d74e6fe32506ff0ca60052b2df
Fedir Smilyanets <fedor.smile@gmail.com>
2018-01-31 17:23:03 +0200

    implemented cli args, mains

---
+++ arguments.js
@@ -0,0 +1,61 @@
+'use strict';
+const ArgumentParser = require('argparse').ArgumentParser;
+
+const m = module.exports = {};
+
+const parser = new ArgumentParser({
+  version: '0.0.1',
+  addHelp:true,
+  description: 'Command line utility for watching and pulling changes in particular branch of repository.\n\nSee http://github.com/ffluffyhedgehog/node-git-watcher'
+});
+/*parser.addArgument(
+  [ '-c', '--config' ],
+  {
+    help: 'Path to a config. Overrides all cli parameters.'
+  }
+);*/
+parser.addArgument(
+  [ '-p', '--path' ],
+  {
+    help: 'Specifies path to repository. ./ by default.'
+  }
+);
+parser.addArgument(
+  [ '-b', '--branch' ],
+  {
+    help: 'Specifies a branch to watch. Master by default'
+  }
+);
+parser.addArgument(
+  [ '-t', '--time' ],
+  {
+    help: 'Specifies how frequently check for changes, in minutes. -t 10 (once every ten minutes) is default.'
+  }
+);
+parser.addArgument(
+  [ '-sB', '--script-before' ],
+  {
+    help: 'Specifies command to execute before pulling. Will be executed in specified path.',
+    required: false,
+  }
+);
+parser.addArgument(
+  [ '-sA', '--script-after' ],
+  {
+    help: 'Specifies command to execute after pulling. Will be executed in specified path.',
+    required: false,
+  }
+);
+const args = parser.parseArgs();
+
+if (!args.path) {
+	args.path = './';
+}
+if (!args.branch) {
+	args.branch = 'master';
+}
+if (!args.time) {
+	args.time = '10';
+}
+
+m.args = () => args;
---
+++ index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+'use strict';
+const shell = require('shelljs');
+const git = require('simple-git');
+const args = require('./arguments.js').args();
+
+// console.log(args)
+shell.cd(args.path);
+// console.log(shell.exec(args.script_before));
+const repo = git(args.path);
+
+function watcher() {
+	setTimeout(() => {
+		
+	}, 0);
+}
+
+
+
+repo.checkIsRepo((err, isRepo) => {
+	if (isRepo) {
+		watcher();
+		setInterval(watcher, args.time*60000);
+	} else {
+		console.log(args.path + ' is not a git repository');
+		process.exit(1);
+	}
+})
